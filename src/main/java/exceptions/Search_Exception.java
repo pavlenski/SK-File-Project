83f28c66ae1fe365b5a37198f7fc176bd625e033
Nikola <sreckonikola@gmail.com>
2019-11-07 23:11:07 +0100

    Search_Exception fix

--- src/main/java/exceptions/Search_Exception.java
+++ src/main/java/exceptions/Search_Exception.java
@@ -2,6 +2,6 @@ package exceptions;
 
 public class Search_Exception extends Exception {
     public Search_Exception() {
-        super("Error while trying to search for the desired item.");
+        super("Error while trying to search the desired item.");
     }
 }
