154dd5e522cf0829f662795e461565063ef87264
paccoditutticapi <p4vlee@gmail.com>
2019-11-07 22:36:04 +0100

    - awee bit exception.

--- src/main/java/exceptions/Search_Exception.java
+++ src/main/java/exceptions/Search_Exception.java
@@ -2,7 +2,6 @@ package exceptions;
 
 public class Search_Exception extends Exception {
     public Search_Exception() {
-        System.out.println("Error while trying to search for the desired item.");
-        System.err.println("Error while trying to search for the desired item.");
+        super("Error while trying to search for the desired item.");
     }
 }
