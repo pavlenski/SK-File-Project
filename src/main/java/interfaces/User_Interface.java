de9e782142ddd1629866da2f7f350d364639b26a
Nikola <sreckonikola@gmail.com>
2019-11-07 12:32:31 +0100

    fix

--- src/main/java/interfaces/User_Interface.java
+++ src/main/java/interfaces/User_Interface.java
@@ -8,9 +8,9 @@ import model.UserPriority;
 
 public interface User_Interface {
 
-    void log_in(String username, String password) throws Log_In_Exception;
+    boolean log_in(String username, String password) throws Log_In_Exception;
 
-    void log_out() throws Log_Out_Exception;
+    boolean log_out() throws Log_Out_Exception;
 
     void create_user(String username, String password, UserPriority priority) throws Create_User_Exception;
 
